3fe9b4bf21aafe0f382cd1bc9870311084ede67a
Eneas U de Queiroz <cote2004-github@yahoo.com>
2018-05-25 23:53:10 -0300

    Adicionada funcao.

--- ibrdtn/daemon/src/security/exchange/DHProtocol.cpp
+++ ibrdtn/daemon/src/security/exchange/DHProtocol.cpp
@@ -363,3 +363,25 @@ namespace dtn
 		}
 	} /* namespace security */
 } /* namespace dtn */
+#if OPENSSL_VERSION_NUMBER < 0x10100000L
+
+void DH_get0_pqg(const DH *dh,
+                 const BIGNUM **p, const BIGNUM **q, const BIGNUM **g)
+{
+    if (p != NULL)
+        *p = dh->p;
+    if (q != NULL)
+        *q = dh->q;
+    if (g != NULL)
+        *g = dh->g;
+}
+
+void DH_get0_key(const DH *dh, const BIGNUM **pub_key, const BIGNUM **priv_key)
+{
+    if (pub_key != NULL)
+        *pub_key = dh->pub_key;
+    if (priv_key != NULL)
+        *priv_key = dh->priv_key;
+}
+
+#endif /* OPENSSL_VERSION_NUMBER */
